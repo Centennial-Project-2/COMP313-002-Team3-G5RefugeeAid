827c38eb36ea52abbbbcd72c8b693ea0a92f1d7e
Nanoteknology <36832557+Nanoteknology@users.noreply.github.com>
2020-10-27 00:09:36 -0400

    Apply Firebase authentication
    Create firebase connection
    Run sample firebase user authentication

---
+++ app/src/main/java/com/comp313002/team3/g5refugeeaid/BaseActivity.java
@@ -0,0 +1,46 @@
+package com.comp313002.team3.g5refugeeaid;
+
+import androidx.annotation.VisibleForTesting;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ProgressBar;
+
+public class BaseActivity extends AppCompatActivity {
+
+    @VisibleForTesting
+    public ProgressBar mProgressBar;
+
+    public void setProgressBar(ProgressBar progressBar) {
+        mProgressBar = progressBar;
+    }
+
+    public void showProgressBar() {
+        if (mProgressBar != null) {
+            mProgressBar.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void hideProgressBar() {
+        if (mProgressBar != null) {
+            mProgressBar.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    public void hideKeyboard(View view) {
+        final InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (imm != null) {
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        hideProgressBar();
+    }
+
+}
--- app/src/main/java/com/comp313002/team3/g5refugeeaid/MainActivity.java
+++ app/src/main/java/com/comp313002/team3/g5refugeeaid/MainActivity.java
@@ -2,13 +2,34 @@ package com.comp313002.team3.g5refugeeaid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+
+    private Button bt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bt = findViewById(R.id.enterButton);
+
+        bt.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.enterButton:
+                Intent intent = new Intent(this, UserLoginActivity.class);
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
     }
 }
