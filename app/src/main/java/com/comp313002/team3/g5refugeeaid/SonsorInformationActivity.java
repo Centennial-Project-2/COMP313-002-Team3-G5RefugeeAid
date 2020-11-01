517be39ca35544fd9bf45b2c0ee1efde867ce285
Nanoteknology <36832557+Nanoteknology@users.noreply.github.com>
2020-11-01 18:16:10 -0500

    resolve errors

--- app/src/main/java/com/comp313002/team3/g5refugeeaid/SonsorInformationActivity.java
+++ app/src/main/java/com/comp313002/team3/g5refugeeaid/SonsorInformationActivity.java
@@ -1,21 +1,8 @@
 package com.comp313002.team3.g5refugeeaid;
 
 import android.os.Bundle;
-import android.support.wearable.activity.WearableActivity;
 import android.widget.TextView;
 
-public class SonsorInformationActivity extends WearableActivity {
+public class SonsorInformationActivity {
 
-    private TextView mTextView;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sonsor_information);
-
-        mTextView = (TextView) findViewById(R.id.text);
-
-        // Enables Always-on
-        setAmbientEnabled();
-    }
 }
