776d0f085ee3c487a13d6bfdad55a2c4f8633161
Nanoteknology <36832557+Nanoteknology@users.noreply.github.com>
2020-11-10 02:38:49 -0500

    Add android tests for user signup

---
+++ app/src/androidTest/java/com/comp313002/team3/g5refugeeaid/MainActivityAndroidTests.java
@@ -0,0 +1,87 @@
+package com.comp313002.team3.g5refugeeaid;
+
+import androidx.test.espresso.intent.Intents;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MainActivityAndroidTests {
+
+    @Rule public ActivityScenarioRule<MainActivity> activityScenarioRule
+            = new ActivityScenarioRule<>(MainActivity.class);
+    @Before
+    public void initialize() {
+        Intents.init();
+    }
+    @Test
+    public void openActivity_SignUpPage() {
+        // click on sign up button
+        onView(withId(R.id.btnSignUp)).perform(click());
+
+        // Check if new activity is opened.
+        intended(hasComponent(SignUpActivity.class.getName()));
+        Intents.release();
+    }
+
+    @Test
+    public void checkVisibility_MainPageViews() {
+
+        Intents.release();
+        // Check if all initial views are shown.
+        onView(withId(R.id.imageView)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+        onView(withId(R.id.btnSignIn)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+        onView(withId(R.id.btnSignUp)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+
+    }
+
+    @Test
+    public void checkVisibility_SignInBox() {
+        Intents.release();
+        // click on sign up button
+        onView(withId(R.id.btnSignIn)).perform(click());
+
+        // Check if sign in box is shown.
+        onView(withId(R.id.txtEmail)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+        onView(withId(R.id.txtPassword)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+        onView(withId(R.id.btnSignInSubmit)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+        onView(withId(R.id.btnCancelSignIn)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+        onView(withId(R.id.btnSignUp)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.GONE)));
+        onView(withId(R.id.btnSignIn)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.GONE)));
+    }
+
+    @Test
+    public void checkVisibility_AfterClickCancelSignIn() {
+        Intents.release();
+        // click on sign up button
+        onView(withId(R.id.btnSignIn)).perform(click());
+        onView(withId(R.id.btnCancelSignIn)).perform(click());
+
+        // Check if sign in box is shown.
+        onView(withId(R.id.txtEmail)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.GONE)));
+        onView(withId(R.id.txtPassword)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.GONE)));
+        onView(withId(R.id.btnSignInSubmit)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.GONE)));
+        onView(withId(R.id.btnCancelSignIn)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.GONE)));
+        onView(withId(R.id.btnSignUp)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+        onView(withId(R.id.btnSignIn)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+    }
+
+
+
+
+}
---
+++ app/src/androidTest/java/com/comp313002/team3/g5refugeeaid/SignUpAndroidTests.java
@@ -0,0 +1,78 @@
+package com.comp313002.team3.g5refugeeaid;
+
+import androidx.test.espresso.ViewAssertion;
+import androidx.test.espresso.contrib.PickerActions;
+import androidx.test.espresso.intent.Intents;
+import androidx.test.espresso.matcher.ViewMatchers;
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+import android.content.Context;
+import android.os.SystemClock;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onData;
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.scrollTo;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.intent.Intents.intended;
+import static androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent;
+import static androidx.test.espresso.matcher.RootMatchers.isPlatformPopup;
+import static androidx.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class SignUpAndroidTests {
+
+    public static final String[] STRING_TO_BE_TYPED = {
+            "TestName", "TestLastName", "United States", "test@testing.com", "647-703-9898", "UN-14556HJ", "pass123"
+    };
+
+    @Rule public ActivityScenarioRule<SignUpActivity> activityScenarioRule
+            = new ActivityScenarioRule<>(SignUpActivity.class);
+    @Before
+    public void initialize() {
+    }
+
+
+    @Test
+    public void createRefugeeUser_sameActivity() {
+        // Type text and then press the button.
+        onData(allOf(is(instanceOf(String.class)))).atPosition(0).perform(click());
+
+        onView(withText("I am a Refugee")).inRoot(isPlatformPopup()).perform(click());
+
+        ///onView(withText("I am a Refugee"))
+                //.check(matches(withText(containsString("I am a Refugee"))));
+        onView(withId(R.id.txtFirstName)).perform(typeText(STRING_TO_BE_TYPED[0]));
+        onView(withId(R.id.txtLastName)).perform(typeText(STRING_TO_BE_TYPED[1]));
+        onView(withId(R.id.bDayPicker)).perform(PickerActions.setDate(2001, 6, 30));
+        onView(withId(R.id.txtNationality)).perform(typeText(STRING_TO_BE_TYPED[2]), closeSoftKeyboard());
+        onView(withId(R.id.txtEmail)).perform(typeText(STRING_TO_BE_TYPED[3]), closeSoftKeyboard());
+        onView(withId(R.id.txtPhoneNumber)).perform(typeText(STRING_TO_BE_TYPED[4]), closeSoftKeyboard());
+        onView(withId(R.id.txtUnNumber)).perform(typeText(STRING_TO_BE_TYPED[5]), closeSoftKeyboard());
+        onView(withId(R.id.txtPassword)).perform(scrollTo(), typeText(STRING_TO_BE_TYPED[6]), closeSoftKeyboard());
+        onView(withId(R.id.txtConfirmPassword)).perform(scrollTo(), typeText(STRING_TO_BE_TYPED[6]), closeSoftKeyboard());
+        onView(withId(R.id.btn_submit)).perform(scrollTo(), click());
+        SystemClock.sleep(3500);
+        onView(withId(R.id.btn_Confirm)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
+    }
+}
